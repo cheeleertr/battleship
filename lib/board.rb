c39c0d088c13e3d22968f763e6e63a90fa74f444
Zach Bergman <bzach634@gmail.com>
2024-04-05 17:56:19 -0600

    refactor: Continue work on #overlap?

--- lib/board.rb
+++ lib/board.rb
@@ -15,7 +15,7 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    if ship.length != coordinates.length || overlap?(coordinates)
+    if ship.length != coordinates.length && overlap?(coordinates)
       false
     elsif
       row = coordinates.map do |coordinate| 
@@ -48,10 +48,10 @@ class Board
 
   def overlap?(coordinates)
     coordinates.each do |coordinate|
-      if @cells[coordinate].empty?
-        return false
-      else 
+      if !@cells[coordinate].empty?
         return true
+      else 
+        false
       end
     end
   end
