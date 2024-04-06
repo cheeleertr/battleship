7eeb771b14a779a76483acab359fbe2adc97e881
Chee Lee <cheeleertr@gmail.com>
2024-04-06 15:33:29 -0500

    feat: add player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,16 @@
 class Player
   attr_reader :ships, :board
 
-  def initialize(ships)
-    @ships = ships
+  def initialize
+    @ships = []
     @board = Board.new
   end
 
   def place_ship(cruiser, coordinates)
     @board.place(cruiser, coordinates)
+  end
+
+  def add_ship(ship)
+    @ships << ship
   end
 end
