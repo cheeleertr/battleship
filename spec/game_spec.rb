86bb474268556e6463ab6ac01991035fcb45ef7c
Chee Lee <cheeleertr@gmail.com>
2024-04-06 12:14:11 -0500

    test: add game test

---
+++ spec/game_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+  before :each do
+    @computer_player = Player.new
+    @player_1 = Player.new
+
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+
+    expect().to be_an_instance_of()
+    end
+
+    # it 'has cells' do
+
+    #   expect().to all(be_a())
+    # end
+
+    # it '' do
+      
+    #   expect().to be_a()
+    # end
+
+    # it '' do
+
+    #   expect().to eq()
+    # end
+  end
+end
