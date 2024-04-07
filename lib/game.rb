679b63baa8e27efc4c7e854a34b6a46c1cdacbb4
Zach Bergman <bzach634@gmail.com>
2024-04-06 18:38:17 -0600

    feat: Add #player_1_place_ships

--- lib/game.rb
+++ lib/game.rb
@@ -26,13 +26,11 @@ class Game
     puts "The Cruiser is three units long and the Submarine is two units long."
     # puts "explain ship placement to player 1"
     puts "ex: cruiser, ['A1', 'B1', 'C1']"
+    player_1_place_ships
     puts @player_1.board.render(true)
-
   end
 
   def computer_player_place_ships
-    
-
     @computer_player.ships.each do |ship|
       coord = random_coordinate(ship)
       @computer_player.board.place(ship, coord)
@@ -40,10 +38,7 @@ class Game
       # occupied = @computer_player.board.cells.find_all do |cell|
       #   cell.ships == ship
     end
-
     # coord == occupied
-
-
   end
 
   def random_coordinate(ship)
@@ -54,11 +49,20 @@ class Game
     sample_coords
   end
 
-  # def computer_placement(ship)
-  #   sample_coords = board.cells.keys.sample(ship.length)
-  #   while @computer_player.board.!valid_placement(ship, sample_coords)
-  #       sample_coords = board.cells.keys.sample(ship.length)
-  #   end
-  #   sample_coords
-  # end
+  def player_1_place_ships
+    @player_1.ships.each do |ship|
+      puts "\n"
+      puts @player_1.board.render(true)
+      puts "\n"
+      puts "Enter the squares for the #{ship} (#{ship.length} spaces): "
+      puts "\n"
+      puts "\n"
+      response = gets.chomp.split(" ")
+      while !@player_1.board.valid_placement?(ship, response)
+        puts "Those are invalid coordinates. Please try again:"
+        response = gets.chomp.split(" ")
+      end
+      @player_1.board.place(ship, response)
+    end
+  end
 end
