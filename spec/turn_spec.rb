3c034f3bf30d1193e35f55467e5d133ab6aa0e82
Zach Bergman <bzach634@gmail.com>
2024-04-06 19:39:42 -0600

    test: Add #display_boards test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -48,8 +48,22 @@ RSpec.describe Turn do
 
             expected = "==============PLAYER BOARD==============\n" \
                         "  1 2 3 4 \nA . . . . \nB S S S . \nC . . . . \nD . . . . \n"
-
+            binding.pry
             expect(@turn.display_player_board).to eq(expected)
         end
     end
+
+    # describe '#display_boards' do
+    #     it 'can display the computer and player boards' do
+    #         @computer_player.board.place(@cruiser, ["A1", "A2", "A3"])
+    #         @player_1.board.place(@cruiser, ["B1", "B2", "B3"])
+
+    #         expected = "=============COMPUTER BOARD=============\n" \
+    #                     "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n" \
+    #                     "==============PLAYER BOARD==============\n" \
+    #                     "  1 2 3 4 \nA . . . . \nB S S S . \nC . . . . \nD . . . . \n"
+
+    #         expect(@turn.display_boards).to eq(expected) #returning nil - how to test?
+    #     end
+    # end
 end
